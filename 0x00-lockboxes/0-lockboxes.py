205d5ad8243c66036c43cdfbd37369198b2c58d7
Malek salem <malek.salem.14@gmail.com>
2020-12-16 00:20:08 +0100

    Update 0-lockboxes.py

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -7,7 +7,7 @@ def canUnlockAll(boxes):
         for i in ibox:
             all_keys.append(i)
     all_keys = set(all_keys)
-    for i in range(1, len(boxes) + 1):
-        if i not in all_keys:
+    for i in range(1, len(boxes)):
+        if i not in all_keys and boxes[i]:
             return False
     return True
