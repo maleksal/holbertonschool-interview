c63f5fa837ad74259e931c23df20646d714b0530
Malek salem <malek.salem.14@gmail.com>
2020-12-16 00:27:52 +0100

    Update 0-lockboxes.py

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 
-
 def canUnlockAll(boxes):
     all_keys = []
-    for ibox in boxes:
-        for i in ibox:
-            all_keys.append(i)
-    all_keys = set(all_keys)
-    for i in range(1, len(boxes)):
-        if i not in all_keys and boxes[i]:
-            return False
-    return True
+    if 1 in boxes[0]:
+        for ibox in boxes:
+            for i in ibox:
+                all_keys.append(i)
+        all_keys = set(all_keys)
+        for i in range(1, len(boxes)):
+            if i not in all_keys and boxes[i]:
+                return False
+        return True
+    return False
