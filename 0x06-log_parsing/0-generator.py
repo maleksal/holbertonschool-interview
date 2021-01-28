90da43c03585c9b523ada5090b7bb3f2ed46b49a
maleksal <malek.salem.14@gmail.com>
2021-01-28 01:00:40 +0100

    add file

---
+++ 0x06-log_parsing/0-generator.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import datetime
+import random
+import sys
+from time import sleep
+
+for i in range(110):
+    sleep(random.random())
+    sys.stdout.write("{:d}.{:d}.{:d}.{:d} - [{}] \"GET /projects/260 HTTP/1.1\" {} {}\n".format(
+        random.randint(1, 255), random.randint(1, 255), random.randint(1, 255), random.randint(1, 255),
+        datetime.datetime.now(),
+        random.choice([200, 301, 400, 401, 403, 404, 405, 500]),
+        random.randint(1, 1024)
+    ))
+    sys.stdout.flush()
