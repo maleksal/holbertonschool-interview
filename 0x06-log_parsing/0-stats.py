f23db7077f141cd4e3be4fb4a7adf86ab394f662
maleksal <malek.salem.14@gmail.com>
2021-02-02 14:34:16 +0100

    fix code

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -1,64 +1,63 @@
 #!/usr/bin/python3
-"""
-Log parsing algorithm module
-"""
-
 import sys
-from signal import SIGINT, signal
 
 
 class Parser:
-    """
+    '''
     Parser class - parses lod data from stdin.
-    """
+
+    '''
     __file_size = 0
     __status_codes = {}
 
     def __init__(self, lines=10):
-        """Set lines && Start listening to cntrl-c."""
+        '''Set lines && Start listening to cntrl-c.'''
         self.lines = lines
-        signal(SIGINT, self.signal_handler)
 
-    def signal_handler(self, signal, frame):
-        """Handles cntr-c signal."""
-        return self.statics()
+    def signal_handler(self):
+        '''Handles cntr-c signal.'''
+        self.build_data(self.__file_size, self.__status_codes)
 
-    def statics(self):
-        """Helper function that builds statics."""
-        scodes = ["{}:{}".format(k, v) for k, v in self.__status_codes.items()]
-        return ("File size: {}".format(self.__file_size), scodes)
+    def statics(self, file_size, status_codes):
+        '''Helper function that builds statics.'''
+        scodes = [f"{k}:{v}" for k, v in self.__status_codes.items()]
+        return (f"File size: {file_size}", scodes)
 
-    def extract_data(self, text):
-        """Extract useful data from string."""
-        occ, idx, i = 0, 0, 0
-        while i < len(text):
-            if occ == 1 and text[i] == '\"':
-                idx = i
+    def extract_data(self, s):
+        '''Extract useful data from string.'''
+        idx = len(s) - 1
+        while idx > 0:
+            if s[idx] == '\"':
                 break
-            if text[i] == '\"':
-                occ += 1
-            i += 1
-        tlist = text[idx + 1:].split()
-        if not len(tlist) > 1:
-            tlist.insert(0, None)
-        return tlist
+            idx -= 1
+        return s[idx + 1:].split()
 
     def start(self):
-        """Process data."""
-        counter = 0
-        for text in sys.stdin:
-            if counter < self.lines:
-                data = self.extract_data(text)
-                if data[0] in self.__status_codes.keys():
-                    self.__status_codes[data[0]] += 1
-                elif data[0]:
-                    self.__status_codes[data[0]] = 1
-                self.__file_size += int(data[1])
-            else:
-                f, s = self.statics()
-                if not print(f) and [print(i) for i in sorted(s)]:
-                    pass
-                counter -= self.lines
-            counter += 1
+        '''Process data.'''
+        try:
+            counter = 1
+            for text in sys.stdin:
+                if counter < self.lines:
+                    data = self.extract_data(text)
+                    print("im", data)
+                    if data[0] in self.__status_codes.keys():
+                        self.__status_codes[data[0]] += 1
+                    elif data[0]:
+                        self.__status_codes[data[0]] = 1
+                    self.__file_size += int(data[1])
+                else:
+                    f, s = self.statics(self.__file_size, self.__status_codes)
+                    if not print(f) and [print(i) for i in sorted(s)]:
+                        pass
+                    counter = 1
+                counter += 1
+
+        except KeyboardInterrupt:
+            self.signal_handler()
+        finally:
+            f, s = self.statics(self.__file_size, self.__status_codes)
+            if not print(f) and [print(i) for i in sorted(s)]:
+                pass
+
 
 Parser().start()
