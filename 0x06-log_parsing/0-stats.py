10e2732c190dea9ca935fda13ea7259126ab5b79
maleksal <malek.salem.14@gmail.com>
2021-02-02 17:47:13 +0100

    fix code

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -3,4 +3,38 @@
 
 
 import sys
-print("hello")
+
+# initialize var
+filesize = 0
+counter = 1
+lines = 10
+status = {}
+
+try:
+    for s in sys.stdin:
+        idx = len(s) - 1
+        while idx > 0:
+            if s[idx] == '\"':
+                break
+            idx -= 1
+        data = s[idx + 1:].split()
+        filesize += int(data[1])
+        if counter < lines:
+            if data[0] in status.keys():
+                status[data[0]] += 1
+            else:
+                status[data[0]] = 1
+        else:
+            print("File size: {}".format(filesize))
+            for k, v in status.items():
+                print("{}: {}".format(k, v))
+            counter = 1
+        counter += 1
+except KeyboardInterrupt as e:
+    print("File size: {}".format(filesize))
+    for k, v in status.items():
+        print("{}: {}".format(k, v))
+finally:
+    print("File size: {}".format(filesize))
+    for k, v in status.items():
+        print("{}: {}".format(k, v))
