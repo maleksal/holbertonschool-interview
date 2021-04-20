50f1886f1101c55f34e35a00d69f4f607ad62d04
maleksal <malek.salem.14@gmail.com>
2021-04-20 16:21:48 +0100

    clean code

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -7,6 +7,4 @@ def canUnlockAll(boxes):
         keys.append(k) for i, box in enumerate(boxes)
         for k in box if k != i and (k not in keys and k <= len(boxes))
     ]
-    if not sum(keys) == sum(range(len(boxes))):
-        return False
-    return True
+    return sum(keys) == sum(range(len(boxes)))
