a6fce60c2854cd15a601fb2cfe7a70597f358d93
Malek salem <malek.salem.14@gmail.com>
2021-04-21 10:21:09 +0100

    reducing complexity

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -20,4 +20,4 @@ def canUnlockAll(boxes):
         keys.append(k) for i, box in enumerate(boxes)
         for k in box if k != i and (k not in keys and k <= len(boxes) - 1)
     ]
-    return sum(keys) == sum(range(len(boxes)))
+    return len(keys) == len(boxes)
