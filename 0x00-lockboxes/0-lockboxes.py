743e50f8b546a6ea890f0ae43d1b5800bf8e582d
Malek salem <malek.salem.14@gmail.com>
2021-04-21 15:41:48 +0100

    update doc

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,17 +1,11 @@
 #!/usr/bin/python3
 """
-Write a method that determines if all the boxes can be opened.
-
-Prototype: def canUnlockAll(boxes)
-- A key with the same number as a box opens that box
-- You can assume all keys will be positive integers
-- There can be keys that do not have boxes
-- The first box boxes[0] is unlocked
+canUnlockAll - determines if all the boxes can be opened.
 """
 
 
 def canUnlockAll(boxes):
-    """Unlock boxes
+    """
     :param boxes: list of lists
     :return True if all boxes can be opened, else return False.
     """
