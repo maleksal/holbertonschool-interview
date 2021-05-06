475026cb7dd789f6c43c89947475b3e612c6218c
maleksal <malek.salem.14@gmail.com>
2021-05-06 22:04:00 +0100

    fix bug

--- 0x02-heap_insert/0-binary_tree_node.c
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -17,6 +17,10 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 
 	new_node->parent = parent;
 	new_node->n = value;
+
+	/* initialize right && left child */
+	new_node->left = NULL;
+	new_node->left = NULL;
 	return (new_node);
 }
 
