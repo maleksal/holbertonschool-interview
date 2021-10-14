15b6d77d9fd5877b0f742387cfee5c2cb843777e
malek salem <malek.salem.14@gmail.com>
2021-10-14 19:39:24 +0100

    add task

---
+++ 0x18-merge_sort/0-merge_sort.c
@@ -0,0 +1,97 @@
+#include "sort.h"
+
+/**
+ * merge - merge the sublist into the list
+ * @start: start index
+ * @mid: middle index
+ * @end: end index
+ * @array: array to be sorted
+ * @temp_array: temp array that holds values
+ *
+ * Return: void
+ */
+void merge(int *array, int *temp_array, size_t start, size_t mid, size_t end)
+{
+size_t i = start;
+size_t j = mid;
+size_t k = start;
+size_t idx;
+
+printf("Merging...\n");
+printf("[left]: ");
+print_array(temp_array + start, mid - start);
+printf("[right]: ");
+print_array(temp_array + mid, end - mid);
+
+while (i < mid && j < end && k < end)
+{
+if (temp_array[i] <= temp_array[j])
+array[k] = temp_array[i++];
+else
+array[k] = temp_array[j++];
+k++;
+}
+while (i < mid)
+array[k++] = temp_array[i++];
+
+while (j < end)
+array[k++] = temp_array[j++];
+
+for (idx = start; idx < end; idx++)
+temp_array[idx] = array[idx];
+
+printf("[Done]: ");
+print_array(array + start, end - start);
+}
+
+/**
+ * acctual_mergesort - split array recursively and merge them back
+ * @start: start
+ * @end: end
+ * @array: array to be sorted
+ * @temp_array: temp array hold values
+ *
+ * Return: void
+ */
+
+void acctual_mergesort(int *array, int *temp_array, size_t start, size_t end)
+{
+size_t mid;
+
+if (end - start < 2)
+return;
+
+mid = (start + end) / 2;
+acctual_mergesort(array, temp_array, start, mid);
+acctual_mergesort(array, temp_array, mid, end);
+merge(array, temp_array, start, mid, end);
+}
+
+/**
+ * intcpy - Generate a copy of a src array.
+ * @src: src array
+ * @len: size of the array
+ *
+ * Return: void
+ */
+int *intcpy(int const *src, size_t len)
+{
+int *p = malloc(len * sizeof(int));
+memcpy(p, src, len * sizeof(int));
+return (p);
+}
+
+/**
+ * merge_sort - sorts an array of integers
+ * @array: array
+ * @size: size of the array
+ *
+ * Return: void
+ */
+void merge_sort(int *array, size_t size)
+{
+int *temp_array;
+
+temp_array = intcpy(array, size);
+acctual_mergesort(array, temp_array, 0, size);
+}
---
+++ 0x18-merge_sort/sort.h
@@ -0,0 +1,11 @@
+#ifndef SORT_H
+#define SORT_H
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+void merge_sort(int *array, size_t size);
+void print_array(const int *array, size_t size);
+
+#endif /* SORT_H */
