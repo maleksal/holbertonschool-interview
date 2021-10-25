558414d21aa5b17e10b7500b7f82cddfcc6836c7
malek salem <malek.salem.14@gmail.com>
2021-10-25 09:44:55 +0100

    add files

---
+++ 0x19-making_change/0-making_change.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Change comes from within
+"""
+
+
+def makeChange(coins, total):
+    """
+    Determine the fewest number of coins needed to meet a given amount.
+    """
+    counter, coins = max(coins), sorted(coins, reverse=True)
+    number_of_coins = 1
+
+    while counter < total:
+
+        if counter + max(coins) > total:
+            coins.pop(0)
+
+        counter += max(coins)
+        number_of_coins += 1
+
+    if counter != total:
+        number_of_coins = -1
+
+    return number_of_coins
