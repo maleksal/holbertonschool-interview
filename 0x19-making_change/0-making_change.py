6da7a3f3cdb97e918e8290300ef61d57162e2f5b
malek salem <malek.salem.14@gmail.com>
2021-10-25 09:45:58 +0100

    add new if statement to the function

--- 0x19-making_change/0-making_change.py
+++ 0x19-making_change/0-making_change.py
@@ -8,6 +8,10 @@ def makeChange(coins, total):
     """
     Determine the fewest number of coins needed to meet a given amount.
     """
+
+    if total <= 0:
+        return 0
+
     counter, coins = max(coins), sorted(coins, reverse=True)
     number_of_coins = 1
 
