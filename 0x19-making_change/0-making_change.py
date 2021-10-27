dd62280a2c2b722593b2ade9bec7ae6b34496b22
malek salem <malek.salem.14@gmail.com>
2021-10-27 17:34:48 +0100

    update code

--- 0x19-making_change/0-making_change.py
+++ 0x19-making_change/0-making_change.py
@@ -13,23 +13,19 @@ def makeChange(coins, total):
         return 0
 
     coins = sorted(coins, reverse=True)
-    number_of_coins = 1
 
-    while max(coins) > total:
-        coins.pop(0)
+    current_total = 0
+    number_of_coins = 0
 
-    counter = max(coins)
+    while current_total < total:
 
-    while coins and counter < total:
-
-        while coins and counter + max(coins) > total:
+        while coins and max(coins) > total \
+                or coins and current_total + max(coins) > total:
             coins.pop(0)
 
-        if coins:
-            counter += max(coins)
-            number_of_coins += 1
+        if not coins:
+            break
+        current_total += max(coins)
+        number_of_coins += 1
 
-    if counter != total:
-        number_of_coins = -1
-
-    return number_of_coins
+    return -1 if current_total != total else number_of_coins
