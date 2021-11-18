a327c66256ebc3f729a054223f6cb4afee4bf651
malek salem <malek.salem.14@gmail.com>
2021-11-18 20:57:43 +0100

    fix pep8 styling

--- 0x1C-island_perimeter/0-island_perimeter.py
+++ 0x1C-island_perimeter/0-island_perimeter.py
@@ -13,6 +13,7 @@ def island_perimeter(grid):
         for cell_idx, cell in enumerate(cell_grid):
             if cell == 1:
                 result += 4
-                result -= 2 if idx > 0 and  grid[idx - 1][cell_idx] == 1 else 0
-                result -= 2 if cell_idx > 0 and grid[idx][cell_idx - 1] == 1 else 0
+                result -= 2 if idx > 0 and grid[idx - 1][cell_idx] == 1 else 0
+                result -= 2 if cell_idx > 0 and\
+                    grid[idx][cell_idx - 1] == 1 else 0
     return result
