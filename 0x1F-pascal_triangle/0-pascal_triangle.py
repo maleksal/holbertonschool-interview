effe271d4f9eb56c110b12d38303636756b86f9a
malek salem <malek.salem.14@gmail.com>
2021-12-09 22:42:22 +0100

    add files

---
+++ 0x1F-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Pascal Triangle
+"""
+
+
+def pascal_triangle(n):
+    """
+    returns a representation of Pascals triangle of n
+    """
+    # input n
+
+    triangle = [[1]]
+    for i in range(1, n):
+        triangle.append([])
+        for j in range(i + 1):
+            triangle[i].append(
+
+                1 if j == 0 or j == i
+                else triangle[i - 1][j - 1] + triangle[i - 1][j]
+
+            )
+    return triangle
