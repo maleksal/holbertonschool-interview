73dcc0a1adb65de7e04919aa6f97206f2bc271f6
Malek salem <malek.salem.14@gmail.com>
2021-12-09 22:47:49 +0100

    Update 0-pascal_triangle.py

--- 0x1F-pascal_triangle/0-pascal_triangle.py
+++ 0x1F-pascal_triangle/0-pascal_triangle.py
@@ -11,13 +11,16 @@ def pascal_triangle(n):
     # input n
 
     triangle = [[1]]
-    for i in range(1, n):
-        triangle.append([])
-        for j in range(i + 1):
-            triangle[i].append(
 
+    for i in range(1, n):
+
+        triangle.append([])
+
+        for j in range(i + 1):
+
+            triangle[i].append(
                 1 if j == 0 or j == i
                 else triangle[i - 1][j - 1] + triangle[i - 1][j]
-
             )
+
     return triangle if n > 0 else []
